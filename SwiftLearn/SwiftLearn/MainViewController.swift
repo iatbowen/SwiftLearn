53efd3b8919941ffeb30d7fc23d5286cd9ba29ae
yexiu <bowenzheng@xiaohongshu.com>
2024-07-18 14:51:49 +0800

    init

---
+++ SwiftLearn/SwiftLearn/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  SwiftLearn
+//
+//  Created by 叶修 on 2024/7/18.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
